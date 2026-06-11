e1d8a727eee2d8cbb0cbf0c8e7b5d298a567dd78
SoftRent <codewithneerb@gmail.com>
2026-06-11 18:22:42 +0530

    Initial commit

---
+++ components/Studio/index.ts
@@ -0,0 +1,18 @@
+export { StudioARScene } from "./StudioARScene";
+export { StudioSceneNavigator } from "./StudioSceneNavigator";
+export type {
+  StudioAnimation,
+  StudioAsset,
+  StudioCollisionBinding,
+  StudioProjectApiResponse,
+  StudioProjectAsset,
+  StudioProjectMeta,
+  StudioProjectOpeningScene,
+  StudioProjectOverview,
+  StudioProjectSceneSummary,
+  StudioSceneCreatedBy,
+  StudioSceneFunction,
+  StudioSceneMeta,
+  StudioSceneResponse,
+  ViroAnimationProp,
+} from "./types";
